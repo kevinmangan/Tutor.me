9f4fc59a537bfd152640c65ce8c770e97829fbd6
Kevin Michael Mangan <kmm2256@columbia.edu>
2013-10-31 01:33:12 -0400

    heroku

---
+++ app/models/PrivateVars.java
@@ -0,0 +1,10 @@
+package models;
+
+import play.db.ebean.Model;
+
+/**
+ * Holds private vars. This file should be listed in .gitignore so that this information doesn't become public on github
+ */
+public abstract class PrivateVars extends Model {
+  public static final String SCRIBBLAR_API_KEY = "99A2F615-092C-19D5-6621C3B336B411A5";
+}
