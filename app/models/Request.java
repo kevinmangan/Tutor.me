8313c3d98fbe01f3bf1ff81e3978942d57642178
Kevin Michael Mangan <kmm2256@columbia.edu>
2013-12-10 17:33:42 -0500

    added Date class

--- app/models/Request.java
+++ app/models/Request.java
@@ -14,6 +14,7 @@ import org.joda.time.format.DateTimeFormatter;
 import play.data.validation.Constraints.Required;
 import play.db.ebean.Model;
 import controllers.Application;
+import java.util.Date;
 
 
 /**
